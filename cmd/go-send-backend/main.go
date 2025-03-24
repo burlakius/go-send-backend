66e997c3b02080a1b6d6b2991393dd0028f6b2d9
Andrii Burlaka <burlaka.ai1@novadigital.com>
2025-03-24 17:00:45 +0200

    Initial commit

---
+++ cmd/go-send-backend/main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	// TODO: Config
+	// TODO: DB
+	// TODO: Auth
+	// TODO: API
+	// TODO: NP connection
+	// TODO: UP connection
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/burlakius/go-send-backend
+
+go 1.23.6
